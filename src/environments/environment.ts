e8bca27f6d2f515f9065f4e83770a65b0484f88b
João Ricardo <joaobarbosa226@gmail.com>
2026-05-30 22:20:04 -0300

    vercel: proxy cors workaround

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  // apiUrl: 'http://localhost:8080/api/v1',
-  apiUrl: 'https://nahora-backend-staging.up.railway.app/api/v1',
+  apiUrl: '/api/v1',
 
   timeout: 5_000,
 };
